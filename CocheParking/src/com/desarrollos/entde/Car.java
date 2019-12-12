fbe1c416ce3f1014ccab87ee82167a71f6a467bc
Tomas <tomas@tomas.com>
2019-12-12 10:21:23 +0100

    Código corregido 12/12/2019

--- CocheParking/src/com/desarrollos/entde/Car.java
+++ CocheParking/src/com/desarrollos/entde/Car.java
@@ -39,11 +39,15 @@ public class Car
 //        turnHeadlightsOff();
     }    
 
-    /**
+    private void printGarage() {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
      * Accelerate the car
      * @throws InterruptedException 
      */
-    public void accelerate() throws InterruptedException{
+    public static void accelerate() throws InterruptedException{
         String c = new String("*");
         String space = new String(" ");
     	for(int i=0; i<5; i++)
@@ -58,8 +62,8 @@ public class Car
      * Slow down the car
      * @throws InterruptedException 
      */
-    public void slowDown() throws InterruptedException{
-    	string c = new string("     *");
+    public static void slowDown() throws InterruptedException{
+    	String c = new String("     *");
     	for(int i=0; i<5; i++)
         {
         	System.out.print(c);
@@ -73,9 +77,9 @@ public class Car
      * Move the car downwards
      * @throws InterruptedException 
      */    
-    public void moveDown() throws InterruptedException{
-    	string c = new string("\t\t\t\t  *"));
-    	for(int i=0, i<5, i++)
+    public static void moveDown() throws InterruptedException{
+    	String c = new String("\t\t\t\t  *");
+    	for(int i=0; i<5; i++)
         {
     		if(i!=4)
     			System.out.println(c);
